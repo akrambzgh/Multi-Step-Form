3602b23f67dfb75704bd7e8148af69271049f177
Akram Bouzoualegh <89752239+akrambzgh@users.noreply.github.com>
2023-02-19 18:55:20 +0100

    removing the ability to pass steps

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,24 +1,4 @@
 let allStepsButtons = document.querySelectorAll(".step-btn");
-let allSteps = document.querySelectorAll(".step-box");
-// allStepsButtons.forEach((stepButton) => {
-//   stepButton.addEventListener("click", (e) => {
-//     allStepsButtons.forEach((stepButton) => {
-//       stepButton.classList.remove("active");
-//     });
-//     e.currentTarget.classList.add("active");
-//     allSteps.forEach((step) => {
-//       if (
-//         stepButton.getAttribute("data-stepType") ==
-//         step.getAttribute("data-sectionType")
-//       ) {
-//         allSteps.forEach((step) => {
-//           step.classList.remove("active");
-//         });
-//         step.classList.add("active");
-//       }
-//     });
-//   });
-// });
 
 let allNextStepButtons = document.querySelectorAll(".next");
 
